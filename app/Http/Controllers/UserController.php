da5546f570fb834e53de89191f31897e45d147c1
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-11-02 22:20:55 +0700

    init commit with docker

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(UserCreateRequest $request)
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password', 1234)),
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function update(UserUpdateRequest $request, $id)
+    {
+        $user = User::find($id);
+
+        $user->update($request->only(['first_name', 'last_name', 'email']));
+
+        return response($user, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}

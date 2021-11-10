1baf0854f8f7e79d6cb765963798dd790a1f0d4c
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-11-10 11:44:03 +0700

    add frontend migration

---
+++ database/migrations/2021_11_10_043105_create_user_roles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('role_id')->constrained('roles')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+        $users = User::all();
+
+        foreach ($users as $user) {
+            DB::table('user_roles')->insert([
+                'user_id' => $user->id,
+                'role_id' => $user->role_id,
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_roles');
+    }
+}

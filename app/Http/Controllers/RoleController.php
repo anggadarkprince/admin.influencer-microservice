5f8e578e3e3ea5f7d08374a11ad1f18902d98d49
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-11-26 12:50:49 +0700

    add role and user filter

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -5,18 +5,40 @@ namespace App\Http\Controllers;
 use App\Http\Resources\RoleResource;
 use App\Models\Role;
 use App\Models\RolePermission;
+use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         Gate::authorize('view', 'roles');
 
-        return RoleResource::collection(Role::orderBy('id', 'desc')->get());
+        $query = Role::query();
+
+        if (!empty($q = $request->get('search'))) {
+            $columns = Schema::getColumnListing('roles');
+            $query->where(function (Builder $query) use ($q, $columns) {
+                foreach ($columns as $column) {
+                    $search = $q;
+                    if (in_array(DB::getSchemaBuilder()->getColumnType('roles', $column), ['date', 'datetime'])) {
+                        try {
+                            $search = Carbon::parse($q)->format('Y-m-d');
+                        } catch (InvalidFormatException $e) {
+                        }
+                    }
+                    $query->orWhere($column, 'LIKE', '%' . trim($search) . '%');
+                }
+            });
+        }
+
+        return RoleResource::collection($query->orderBy('id', 'desc')->get());
     }
 
     public function store(Request $request)
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,20 +8,45 @@ use App\Http\Requests\UserCreateRequest;
 use App\Http\Requests\UserUpdateRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Carbon\Carbon;
+use Carbon\Exceptions\InvalidFormatException;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         Gate::authorize('view', 'users');
 
-        $users = User::orderBy('created_at', 'desc')->paginate();
+        $query = User::query();
 
-        return UserResource::collection($users);
+        if (!empty($q = $request->get('search'))) {
+            $columns = Schema::getColumnListing('users');
+            $query->where(function (Builder $query) use ($q, $columns) {
+                foreach ($columns as $column) {
+                    $search = $q;
+                    if (in_array(DB::getSchemaBuilder()->getColumnType('users', $column), ['date', 'datetime'])) {
+                        try {
+                            $search = Carbon::parse($q)->format('Y-m-d');
+                        } catch (InvalidFormatException $e) {
+                        }
+                    }
+                    $query->orWhere($column, 'LIKE', '%' . trim($search) . '%');
+                }
+            });
+        }
+        if (!empty($roleId = $request->get('role'))) {
+            $query->where('role_id', $roleId);
+        }
+
+        return UserResource::collection($query->orderBy('created_at', 'desc')->paginate());
     }
 
     public function show($id)
